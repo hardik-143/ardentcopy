73bd967161ad0fda893920eafeb56267f2bd2d35
Hardik <dhardik1430@gmail.com>
2026-05-24 23:43:21 +0530

    Remove Vercel preview deployment check and set stega.enabled to true

--- utils/sanity/client.ts
+++ utils/sanity/client.ts
@@ -4,9 +4,6 @@ import { createClient } from "next-sanity";
 
 import { env } from "../env/client";
 
-const isVercelPreviewDeployment = () =>
-  typeof process !== "undefined" && process.env.VERCEL_ENV === "preview";
-
 const clientConfig = {
   projectId: env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: env.NEXT_PUBLIC_SANITY_DATASET,
@@ -14,7 +11,7 @@ const clientConfig = {
   useCdn: env.NODE_ENV === "production",
   stega: {
     studioUrl: "/studio",
-    enabled: isVercelPreviewDeployment(),
+    enabled: true,
   },
 } as const;
 
